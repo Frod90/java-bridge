766f3bd11bd0b1aa1bb0c3ff6804ffab3250b9a7
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-04-21 16:21:51 +0900

    Feat: implement make bridge function
    다리를 만드는 기능 구현
    위, 아래 한 쪽만 이동할 수 있는 다리의 구역을 만드는 기능 구현
    다리의 숫자 이동 옵션을 검증하는 기능 구현
    다리의 숫자 이동 옵션을 문자 이동 옵션으로 변경하는 기능 구현
    다리의 문자 이동 옵션을 검증하는 기능 구현

---
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -0,0 +1,69 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bridge.BridgeNumberGenerator;
+import bridge.constant.BridgeMoveOption;
+import bridge.constant.ErrorMessage;
+
+public class BridgeMaker {
+
+	private final BridgeNumberGenerator bridgeNumberGenerator;
+
+	public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+		this.bridgeNumberGenerator = bridgeNumberGenerator;
+	}
+
+	public List<String> makeBridge(int size) {
+
+		List<String> bridge = new ArrayList<>();
+
+		for (int i = 0; i < size; i++) {
+
+			String section = makeBridgeSection();
+			bridge.add(section);
+		}
+
+		return bridge;
+	}
+
+	private String makeBridgeSection() {
+
+		int moveOptionNumber = bridgeNumberGenerator.generate();
+		validateMoveOptionRange(moveOptionNumber);
+
+		String moveOptionSignature = toStringMoveOption(moveOptionNumber);
+		validateMoveOptionSignature(moveOptionSignature);
+
+		return moveOptionSignature;
+	}
+
+	private void validateMoveOptionSignature(String moveStringOption) {
+
+		if (moveStringOption.equals(BridgeMoveOption.FAIL.getSignatureOption())) {
+			throw new IllegalStateException(ErrorMessage.MOVE_OPTION_SIGNATURE);
+		}
+	}
+
+	private String toStringMoveOption(int moveNumberOption) {
+
+		if (moveNumberOption == BridgeMoveOption.UP.getNumberOption()) {
+			return BridgeMoveOption.UP.getSignatureOption();
+		}
+
+		if (moveNumberOption == BridgeMoveOption.DOWN.getNumberOption()) {
+			return BridgeMoveOption.DOWN.getSignatureOption();
+		}
+
+		return BridgeMoveOption.FAIL.getSignatureOption();
+	}
+
+	private void validateMoveOptionRange(int moveOption) {
+
+		if (moveOption != BridgeMoveOption.UP.getNumberOption()
+			&& moveOption != BridgeMoveOption.DOWN.getNumberOption()) {
+			throw new IllegalStateException(ErrorMessage.MOVE_OPTION_NUMBER);
+		}
+	}
+}
