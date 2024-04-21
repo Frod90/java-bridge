49d2a794447714737f074b3b51560e49f8a8a58a
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-04-21 16:18:38 +0900

    Feat: implement bridge move option error message
    다리를 생성할 때 오류가 발생할 경우를 표현할 메시지를 상수로 표현

---
+++ src/main/java/bridge/constant/ErrorMessage.java
@@ -0,0 +1,9 @@
+package bridge.constant;
+
+public class ErrorMessage {
+
+	private static final String ERROR_INDICATION = "[ERROR]";
+	public static final String MOVE_OPTION_NUMBER = ERROR_INDICATION + " 지정된 다리의 이동 옵션 번호가 아닙니다.";
+	public static final String MOVE_OPTION_SIGNATURE = ERROR_INDICATION + " 지정된 다리의 이동 옵션 표현이 아닙니다.";
+
+}
