deb110bd1b455b58dffa68a4138aa6ab79c763c4
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-04-22 00:01:34 +0900

    Feat: implement make map of bridge function
    이동 횟수 까지의 다리 상황을 나타내는 맵을 만드는 기능 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -2,7 +2,17 @@ package bridge.domain;
 
 import java.util.List;
 
+import bridge.constant.BridgeMoveOption;
+
 public class BridgeGame {
+
+	private final String START_MAP_FORMAT = "[";
+	private final String END_MAP_FORMAT = "]";
+	private final String SEPARATOR_MAP_FORMAT = "|";
+	private final String RIGHT_MAP_SECTION_SIGN = " O ";
+	private final String WRONG_MAP_SECTION_SIGN = " X ";
+	private final String UNSELECTED_MAP_SECTION_SIGN = "   ";
+	private final int INDEX_CONVERTER = -1;
 
 	private final List<String> bridge;
 	private int moveCount;
@@ -17,7 +27,7 @@ public class BridgeGame {
 	public void move(String moveOptionSignature) {
 
 		moveCount++;
-		if (moveOptionSignature.equalsIgnoreCase(bridge.get(moveCount - 1))) {
+		if (moveOptionSignature.equalsIgnoreCase(bridge.get(moveCount + INDEX_CONVERTER))) {
 			moveSuccessSign = true;
 			return;
 		}
@@ -33,6 +43,72 @@ public class BridgeGame {
 
 		moveCount = 0;
 		tryCount++;
+	}
+
+	public String getMap() {
+		StringBuilder upSideMap = new StringBuilder(START_MAP_FORMAT);
+		StringBuilder downSideMap = new StringBuilder(START_MAP_FORMAT);
+
+		for (int i = 0; i < moveCount - 1; i++) {
+			makeUpSideSection(upSideMap, i).append(SEPARATOR_MAP_FORMAT);
+			makeDownSideSection(downSideMap, i).append(SEPARATOR_MAP_FORMAT);
+		}
+
+		makeUpSideLastSection(upSideMap).append(END_MAP_FORMAT);
+		makeDownSideLastSection(downSideMap).append(END_MAP_FORMAT);
+		return upSideMap.append("\n").append(downSideMap).toString();
+	}
+
+	private StringBuilder makeUpSideLastSection(StringBuilder upSideMap) {
+
+		if (moveSuccessSign && bridge.get(moveCount + INDEX_CONVERTER).equals(BridgeMoveOption.UP.getSignatureOption())) {
+			return upSideMap.append(RIGHT_MAP_SECTION_SIGN);
+		}
+
+		if (!moveSuccessSign && bridge.get(moveCount + INDEX_CONVERTER).equals(BridgeMoveOption.DOWN.getSignatureOption())) {
+			return upSideMap.append(WRONG_MAP_SECTION_SIGN);
+		}
+
+		return upSideMap.append(UNSELECTED_MAP_SECTION_SIGN);
+	}
+
+	private StringBuilder makeDownSideLastSection(StringBuilder downSideMap) {
+
+		if (moveSuccessSign && bridge.get(moveCount + INDEX_CONVERTER).equals(BridgeMoveOption.DOWN.getSignatureOption())) {
+			return downSideMap.append(RIGHT_MAP_SECTION_SIGN);
+		}
+
+		if (!moveSuccessSign && bridge.get(moveCount + INDEX_CONVERTER).equals(BridgeMoveOption.UP.getSignatureOption())) {
+			return downSideMap.append(WRONG_MAP_SECTION_SIGN);
+		}
+
+		return downSideMap.append(UNSELECTED_MAP_SECTION_SIGN);
+	}
+
+	private StringBuilder makeUpSideSection(StringBuilder upSideMap, int index) {
+
+		if (bridge.get(index).equals(BridgeMoveOption.UP.getSignatureOption())) {
+			upSideMap.append(RIGHT_MAP_SECTION_SIGN);
+		}
+
+		if (bridge.get(index).equals(BridgeMoveOption.DOWN.getSignatureOption())) {
+			upSideMap.append(UNSELECTED_MAP_SECTION_SIGN);
+		}
+
+		return upSideMap;
+	}
+
+	private StringBuilder makeDownSideSection(StringBuilder downSideMap, int index) {
+
+		if (bridge.get(index).equals(BridgeMoveOption.DOWN.getSignatureOption())) {
+			return downSideMap.append(RIGHT_MAP_SECTION_SIGN);
+		}
+
+		if (bridge.get(index).equals(BridgeMoveOption.UP.getSignatureOption())) {
+			return downSideMap.append(UNSELECTED_MAP_SECTION_SIGN);
+		}
+
+		return downSideMap;
 	}
 
 	public boolean isCrossBridge() {
