5c450a416d4b93c064d5aa2412bd8eef805c5cc8
frod <won1680@naver.com>
2024-04-22 11:34:23 +0900

    Style: change import path

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -3,7 +3,6 @@ package bridge.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import bridge.BridgeNumberGenerator;
 import bridge.constant.BridgeMoveOption;
 import bridge.constant.ErrorMessage;
 
