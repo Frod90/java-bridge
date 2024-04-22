ea0a5fdf2df1877bdad9f41efd849f6793f6dffc
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-04-22 23:00:27 +0900

    Feat: implement numeric input validation function
    사용자가 다리의 길이를 입력할 때 숫자만 입력하게 하는 기능 구현(잘못 입력할 시 반복해서 입력)

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,41 +1,48 @@
 package bridge.view;
 
+import java.util.regex.Pattern;
+
+import bridge.constant.ErrorMessage;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
+	
+	public int readBridgeSize() {
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    public int readBridgeSize() {
+		do {
+			try {
+				String inputBridgeSize = validateNumericInput();
+				return Integer.parseInt(inputBridgeSize);
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		} while (true);
+	}
 
-        System.out.println();
-        System.out.println("다리의 길이를 입력해주세요.");
-        String inputBridgeSize = Console.readLine().trim();
+	private String validateNumericInput() {
+		final Pattern NUMBER_PATTERN = Pattern.compile("^[\\d]*$");
 
-        return Integer.parseInt(inputBridgeSize);
-    }
+		System.out.println();
+		System.out.println("다리의 길이를 입력해주세요.");
+		String numericInput = Console.readLine().trim();
 
+		if (!NUMBER_PATTERN.matcher(numericInput).matches()) {
+			throw new IllegalArgumentException(ErrorMessage.NUMERIC_INPUT);
+		}
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
-    public String readMoving() {
+		return numericInput;
+	}
 
-        System.out.println();
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        return Console.readLine().trim();
-    }
+	public String readMoving() {
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
-    public String readGameCommand() {
-        System.out.println();
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        return Console.readLine().trim();
-    }
+		System.out.println();
+		System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+		return Console.readLine().trim();
+	}
+
+	public String readGameCommand() {
+		System.out.println();
+		System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+		return Console.readLine().trim();
+	}
 }
